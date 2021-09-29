e97446a9eed384857525504bbd755ed377749dd3
Liam Bigelow <liam@cloudcannon.com>
2021-09-29 15:02:13 +1300

    Initial commit

---
+++ ac-site/src/.eleventy.js
@@ -0,0 +1,12 @@
+const pluginBookshop = require("@bookshop/eleventy-bookshop");
+const pluginCloudCannonBookshop = require("@bookshop/cloudcannon-eleventy-bookshop");
+
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(pluginBookshop({
+		bookshopLocations: ["../../ac-components"]
+	}));
+
+	eleventyConfig.addPassthroughCopy('assets');
+
+	eleventyConfig.addPlugin(pluginCloudCannonBookshop);
+};
