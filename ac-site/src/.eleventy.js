d1ff17bd89814d936812a561e1022d4926edca41
Liam Bigelow <liam@cloudcannon.com>
2021-10-04 11:52:54 +1300

    Add CloudCannon fix
    Hide unused files
    
    Pass through _cloudcannon directory
    
    Bookshop beta.27

--- ac-site/src/.eleventy.js
+++ ac-site/src/.eleventy.js
@@ -1,12 +1,16 @@
 const pluginBookshop = require("@bookshop/eleventy-bookshop");
 const pluginCloudCannonBookshop = require("@bookshop/cloudcannon-eleventy-bookshop");
+const path = require('path');
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(pluginBookshop({
 		bookshopLocations: ["../../ac-components"]
 	}));
 
-	eleventyConfig.addPassthroughCopy('assets');
+	// passthroughCopy is relative to the CWD, so we need to use the path when on CloudCannon
+	const assetPath = (input) =>  path.join(process.env['CC_ELEVENTY_INPUT'] || '', input);
+	eleventyConfig.addPassthroughCopy(assetPath('assets'));
+	eleventyConfig.addPassthroughCopy(assetPath('_cloudcannon'));
 
 	eleventyConfig.cloudcannonOptions = {
 		dir: {
